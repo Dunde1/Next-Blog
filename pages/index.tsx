72f60f76575a044988dc5cca58b545f961ff83b0
origin <chldlsrb1000@naver.com>
2022-02-27 21:45:12 +0900

    index 페이지 서버사이드 프롭 생성 - 데이터베이스에 저장된 방문자 데이터 생성 및 가져오기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,45 @@
-import type { NextPage } from 'next';
+import { PrismaClient } from '@prisma/client';
+import type { GetServerSideProps, NextPage } from 'next';
 import HeadComponent from '../components/HeadComponent';
-import Home from '../components/index/Home';
+import Home, { HomeProps } from '../components/index/Home';
 import Info from '../components/index/Info';
+import Posts from '../components/index/Posts';
 import ScrollButton from '../components/ScrollScreen';
+import { getNowDate } from '../utils/dateUtil';
 
-const Posts = () => {
-  return <>posts</>;
-};
-
-const Index: NextPage = () => {
+const Index: NextPage<HomeProps> = ({ today, total }) => {
   const screenDescription = ['home', 'information', 'blog posts'];
 
   return (
     <div className="index">
       <HeadComponent title="Main" description="Main page for introduction and navigation" />
       <ScrollButton screenDescription={screenDescription}>
-        <Home />
+        <Home today={today} total={total} />
         <Info />
         <Posts />
       </ScrollButton>
       <style jsx>{``}</style>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const prisma = new PrismaClient();
+
+  const { year, month, date } = getNowDate(new Date());
+  const dateString = `${year}${month}${date}`;
+
+  await prisma.visit.create({ data: { date: dateString, host: req.headers.host ?? 'undefined' } });
+
+  const today: number = await prisma.visit.count({ where: { date: { equals: dateString } } });
+  const total: number = await prisma.visit.count();
+
+  return {
+    props: {
+      today,
+      total,
+    },
+  };
 };
 
 export default Index;
