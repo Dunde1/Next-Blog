92efdb6db4601e6fa66a2ba0efd4227f83f8effc
origin <chldlsrb1000@naver.com>
2022-03-03 00:02:48 +0900

    key 숨기기 용 rewrites 추가 - 기존 url에서 query 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  rewrites: async () => {
+    return [
+      {
+        source: '/get/posts',
+        destination: `/api/posts?get_key=${process.env.GET_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
