2291785b7615584f18e8557e4fc90f6d76e66621
origin <chldlsrb1000@naver.com>
2022-03-04 00:30:25 +0900

    최하단 로딩 표시 생성 - 최하단에 스크롤이 도달하였을 때 로딩 or 컨텐츠 없음 표시 추가

--- components/posts/PostBody.tsx
+++ components/posts/PostBody.tsx
@@ -23,7 +23,7 @@ export type postInfoType = {
 };
 
 export const postsAtom = atom<postsType[]>({ key: 'results', default: [] });
-export const postInfoAtom = atom<postInfoType>({ key: 'postInfo', default: { nextCursor: undefined, hasMore: false } });
+export const postInfoAtom = atom<postInfoType>({ key: 'postInfo', default: { nextCursor: undefined, hasMore: true } });
 
 const PostBody = () => {
   const [posts, setPosts] = useRecoilState(postsAtom);
@@ -33,14 +33,17 @@ const PostBody = () => {
   const mainRef = createRef<HTMLDivElement>();
   const footerRef = createRef<HTMLDivElement>();
 
-  const footerCheck = async (footer: HTMLDivElement, main: HTMLDivElement) => {
-    if (footer.getBoundingClientRect().y - (window.innerHeight - main.getBoundingClientRect().y) <= 0 && !isLoading && postInfo.hasMore)
-      requestPosts(postInfo.nextCursor);
+  const footerCheck = async () => {
+    const [main, footer] = [mainRef.current, footerRef.current];
+    if (!(main && footer)) return;
+    const footerY = footer.getBoundingClientRect().y;
+    const mainY = main.getBoundingClientRect().y;
+    if (footerY - (window.innerHeight - mainY) <= 0 && !isLoading && postInfo.hasMore) requestPosts();
   };
 
-  const requestPosts = async (cursor?: string) => {
+  const requestPosts = async () => {
     setIsLoading(true);
-    const { nextCursor, hasMore, results } = await getPosts(searchList, cursor);
+    const { nextCursor, hasMore, results } = await getPosts(searchList, postInfo.nextCursor);
     results && setPosts([...posts, ...results]);
     setPostInfo({ nextCursor, hasMore });
     setIsLoading(false);
@@ -51,15 +54,11 @@ const PostBody = () => {
   }, []);
 
   useEffect(() => {
-    if (footerRef.current && mainRef.current !== null) footerCheck(footerRef.current, mainRef.current);
-  }, []);
-
-  const onScroll = (e: UIEvent) => {
-    if (footerRef.current && mainRef.current !== null) footerCheck(footerRef.current, mainRef.current);
-  };
+    footerCheck();
+  }, [isLoading]);
 
   return (
-    <main onScroll={onScroll} ref={mainRef}>
+    <main onScroll={footerCheck} ref={mainRef}>
       <ul>
         {posts.map((v, i) => {
           const createDate = getParsingDate(new Date(v.created));
@@ -86,7 +85,7 @@ const PostBody = () => {
             </li>
           );
         })}
-        <div className="footer" ref={footerRef} />
+        <div className={`footer ${isLoading ? 'loading' : ''} ${postInfo.hasMore ? '' : 'no-content'}`} ref={footerRef} />
       </ul>
 
       <style jsx>
@@ -120,16 +119,68 @@ const PostBody = () => {
             background-color: darkgray;
           }
 
+          ul {
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: wrap;
+            gap: 20px;
+          }
+
           li {
-            height: 100px;
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            border: 2px solid gray;
+            border-radius: 10px;
           }
 
           .footer {
             position: relative;
             display: flex;
-            width: 10px;
-            height: 10px;
-            background-color: black;
+            justify-content: center;
+            align-items: center;
+            width: 200px;
+            height: 100px;
+          }
+
+          .footer.loading::after {
+            content: '';
+            width: 30px;
+            height: 30px;
+            background-color: transparent;
+            border: 5px solid transparent;
+            border-top: 5px solid navy;
+            border-bottom: 5px solid crimson;
+            border-radius: 50%;
+            animation: footer-loading-one 1.5s ease-in-out infinite, footer-loading-two 1.5s ease-in-out infinite;
+          }
+
+          @keyframes footer-loading-one {
+            0% {
+              transform: rotateZ(0deg);
+            }
+            50% {
+              transform: rotateZ(180deg);
+            }
+          }
+
+          @keyframes footer-loading-two {
+            50% {
+              transform: rotateZ(180deg);
+            }
+            100% {
+              transform: rotateZ(360deg);
+            }
+          }
+
+          .footer.no-content::after {
+            content: 'NO MORE CONTENT';
+            white-space: nowrap;
+            font-family: netmarble;
           }
         `}
       </style>
