a880e592356dad98721642f56e70bfdc95787be4
origin <chldlsrb1000@naver.com>
2022-03-03 22:30:22 +0900

    이미지 타입 추가

--- utils/useNotion.ts
+++ utils/useNotion.ts
@@ -1,6 +1,13 @@
 import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
 import { searchDB } from './notionUtil';
 
+type iconType = null | {
+  type: 'file';
+  file: {
+    url: string;
+  };
+};
+
 const PAGE_SIZE = 10;
 
 const getSearchData = async (filters: string[], nextCursor?: string) => {
@@ -17,20 +24,22 @@ const getSearchData = async (filters: string[], nextCursor?: string) => {
   const parsingData =
     getData.results?.map((result: any) => {
       const { category, tag, title, description } = result.properties;
-      const { created_time, last_edited_time, url }: { created_time: string; last_edited_time: string; url: string } = result;
+      const { created_time, last_edited_time, icon, url }: { created_time: string; last_edited_time: string; icon: iconType; url: string } = result;
       const { name, color }: { name: string; color: string } = category.select;
       const { multi_select }: { multi_select: { name: string; color: string } } = tag;
       const created = new Date(created_time);
       const lastEdited = new Date(last_edited_time);
+      const image: string | null = icon ? (icon.type === 'file' ? icon.file.url : null) : null;
 
       return {
         created: created.setHours(created.getHours() + 9),
         lastEdited: lastEdited.setHours(lastEdited.getHours() + 9),
+        image,
         category: { name, color },
         tag: multi_select,
         title: title.title[0].plain_text as string,
         description: description.rich_text[0].plain_text as string,
-        url: url,
+        url,
       };
     }) ?? [];
 
