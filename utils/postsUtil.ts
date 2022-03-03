c7f5bbf5897c9058bf24a50cfe61b16d42bf5d2a
origin <chldlsrb1000@naver.com>
2022-03-03 22:27:40 +0900

    오류 수정 - cursor 양쪽 큰 따옴표 추가

--- utils/postsUtil.ts
+++ utils/postsUtil.ts
@@ -1,7 +1,7 @@
 const getPosts = async (filters: string[], cursor?: string) => {
   const url = '/get/posts';
   const filtersQuery = `filters=${JSON.stringify(filters.map((filter) => filter.replace('#', '_')))}`;
-  const result = await fetch(`${url}?${filtersQuery}${cursor ? `&cursor=${cursor}` : ''}`, { method: 'GET' });
+  const result = await fetch(`${url}?${filtersQuery}${cursor ? `&cursor=${JSON.stringify(cursor)}` : ''}`, { method: 'GET' });
 
   return result.json();
 };
