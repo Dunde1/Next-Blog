344deb36857e7ff76c9539c14afe1bbc93696497
origin <chldlsrb1000@naver.com>
2022-03-28 22:27:11 +0900

    바닥 material 추가

--- pages/projects/threejs/tutorial.tsx
+++ pages/projects/threejs/tutorial.tsx
@@ -1,11 +1,14 @@
 import { NextPage } from 'next';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useLoader } from '@react-three/fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
 import { Physics, useBox, usePlane } from '@react-three/cannon';
+import { RepeatWrapping, TextureLoader } from 'three';
 import HeadComponent from '../../../components/HeadComponent';
+import { Suspense } from 'react';
 
 const Box = () => {
   const [ref, api] = useBox(() => ({ mass: 2 }));
+
   return (
     <mesh
       onClick={() => {
@@ -22,10 +25,15 @@ const Box = () => {
 
 const Plane = () => {
   const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
+  const colorMap = useLoader(TextureLoader, '/images/projects/threejs/materials/brick_pavement_tiled.jpg');
+  colorMap.wrapS = RepeatWrapping;
+  colorMap.wrapT = RepeatWrapping;
+  colorMap.repeat.set(10, 10);
+
   return (
     <mesh ref={ref} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
+      <meshStandardMaterial attach="material" map={colorMap} />
     </mesh>
   );
 };
@@ -40,7 +48,7 @@ const Tutorial: NextPage = () => {
           <OrbitControls />
           <ambientLight intensity={0.5} />
           <Stars />
-          <spotLight position={[10, 15, 10]} angle={0.3} />
+          <spotLight position={[10, 10, 10]} angle={1} />
           <Physics>
             <Box />
             <Plane />
